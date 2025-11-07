ed09d5514f12d1c41827febb81c6a87af89fa0eb
megashambala <eir1003@alu.ubu.es>
2025-11-07 11:56:42 +0100

    ya funciona mandar mensajes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from src.prueba import *
-def main():
+def main(test):
     while(True):
         message_personalized = input()
         if isinstance(message_personalized,(str)):
-            message_personalized.send_message()
+            test.send_message(test.brodcastfffffffff,message_personalized)
         else:
             if message_personalized==0:
                 break
 if __name__ == "__main__":
     test=MQTT_Meshtastic()
-    test=main()
+    main(test)
