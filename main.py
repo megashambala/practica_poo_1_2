a9a90dfdbacab765ee3a164113b74e4ec79e8b74
megashambala <eir1003@alu.ubu.es>
2025-11-17 10:02:30 +0100

    probando log main

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from src.prueba import *
 def main(test):
     while(True):
-        message_personalized = input()
+        message_personalized = input("Escribe aqui:")
         if isinstance(message_personalized,(str)):
-            test.send_message(test.brodcastfffffffff,message_personalized)
+            test.send_message(test.brodcast,message_personalized)
         else:
             if message_personalized==0:
                 break
+        test.process_file(test.filename)
 if __name__ == "__main__":
     test=MQTT_Meshtastic()
     main(test)
